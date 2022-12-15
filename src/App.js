7a9c57ab491626ef21a18ee2a92dcb2440ac35ff
Manoj Soni <manojncsoni@gmail.com>
2022-12-15 17:59:57 +0530

    commit for branch 5

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Hi, My name is manoj</h1>
       <h3>This is new branch Branch5</h3>
+      <h3>this line added in p2 branch</h3>
     </div>
   );
 }
