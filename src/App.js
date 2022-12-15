30cf9757b2ef9fbc7154743499a739ff30bc95e6
Manoj Soni <manojncsoni@gmail.com>
2022-12-15 18:01:44 +0530

    added another text in p2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Hi, My name is manoj</h1>
       <h3>This is new branch Branch5</h3>
       <h3>this line added in p2 branch</h3>
+      <h3>another text added in p2 branch</h3>
     </div>
   );
 }
